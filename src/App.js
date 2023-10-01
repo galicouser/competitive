e504b8da5475509c9e17d0a444b5cd8dc8757da4
pauldidonatojr <pjdidonatojr@gmail.com>
2023-10-01 11:12:35 -0400

    push me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS here if you have one.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Welcome to My React App</h1>
+
+      {/* Embedded Presentation Component */}
+      <div className="presentation-container">
+        <iframe
+          src="https://www.slideshare.net/slideshow/embed_code/key/1kpJeWtppXzNp9?hostedIn=slideshare&page=upload"
+          width="600"
+          height="400"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+          scrolling="no"
+          style={{border: '1px solid #CCC', borderWidth: '1px', marginBottom: '5px', maxWidth: '100%'}}
+          allowFullScreen // Corrected to allowFullScreen for React
         >
-          Learn React
-        </a>
-      </header>
+        </iframe>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
